f1b8e5b70fa63f8032fb55bc39c8b39458fcc3a2
PamelaKlapp <pameklapp@gmail.com>
2022-06-29 18:02:54 +0200

    import, added .isRequired & React Bootstrap
    Add .isRequired to Director & Genre
    Import Button & Card (React Boots.)
    Add Card (React Bootstrap)

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
 
     return (
-      <div onClick={() => onMovieClick(movie)} className="movie-card">{movie.Title}</div>
+      <Card>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+        </Card.Body>
+      </Card>
     );
   }
 }
@@ -19,12 +28,12 @@ MovieCard.propTypes = {
     Genre: PropTypes.shape({
       Name: PropTypes.string,
       Description: PropTypes.string
-    }),
+    }).isRequired,
     Director: PropTypes.shape({
       Name: PropTypes.string,
       Bio: PropTypes.string
-    })
+    }).isRequired,
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  onMovieClick: PropTypes.func.isRequired,
 };
 export default MovieCard;
