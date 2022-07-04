5f83c71565dc1d602993ff5317e1c4d9112c3cd0
PamelaKlapp <pameklapp@gmail.com>
2022-07-04 17:15:08 +0200

    Format Document

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export function LoginView(props) {
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,26 +18,32 @@ export function LoginView(props) {
   const openRegView = (e) => {
     e.preventDefault();
     props.onRegistration(true);
-  }
+  };
 
   return (
     <Form>
-    <Form.Group controlId="formUsername">
-      <Form.Label>Username:</Form.Label>
-      <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-    </Form.Group>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </Form.Group>
 
-    <Form.Group controlId="formPassword">
-      <Form.Label>Password:</Form.Label>
-      <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-    </Form.Group>
-    <Button variant="primary" type="submit" onClick={handleSubmit}>
-      Submit
-    </Button>
-    <Button variant="primary" type="button" onClick={openRegView}>
-      Sign Up
-    </Button>
-  </Form>
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={handleSubmit}>
+        Submit
+      </Button>
+      <Button variant="primary" type="button" onClick={openRegView}>
+        Sign Up
+      </Button>
+    </Form>
   );
 }
 
@@ -45,4 +51,3 @@ LoginView.propTypes = {
   onRegistration: PropTypes.func.isRequired,
   onLoggedIn: PropTypes.func.isRequired,
 };
-
