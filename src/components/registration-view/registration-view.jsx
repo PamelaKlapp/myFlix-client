9cffebb9cee720996048487cd94fb8381b97c78a
PamelaKlapp <pameklapp@gmail.com>
2022-07-05 13:51:40 +0200

    add navbar

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,5 +1,18 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import {
+  Form,
+  Button,
+  Card,
+  CardGroup,
+  Container,
+  Col,
+  Row,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
+import "./registration-view.scss";
 
 export function RegistrationView(props) {
   const [username, setUsername] = useState("");
@@ -13,43 +26,87 @@ export function RegistrationView(props) {
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <label>
-        Email:
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <label>
-        Birthday:
-        <input
-          type="birthday"
-          value={birthday}
-          onChange={(e) => setBirthday(e.target.value)}
-        />
-      </label>
-      <button type="submit" onClick={handleSubmit}>
-        Submit
-      </button>
-    </form>
+    <Container>
+      <Navbar className="nav-bg-color" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">MyFlix MOVIE</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">HOME</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container className="main-container">
+        <Row>
+          <Col>
+            <CardGroup>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Make an account</Card.Title>
+                  <Form>
+                    <Form.Group>
+                      <Form.Label>Username:</Form.Label>
+                      <Form.Control
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                        placeholder="Enter Username"
+                      />
+                    </Form.Group>
+
+                    <Form.Group>
+                      <Form.Label>Password:</Form.Label>
+                      <Form.Control
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        placeholder="Enter your Password"
+                      />
+                      <Form.Text className="text-muted">
+                        Password must contain a minimun of 8 charachters
+                      </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group>
+                      <Form.Label>Email:</Form.Label>
+                      <Form.Control
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        placeholder="Enter Email"
+                      />
+                    </Form.Group>
+
+                    <Form.Group>
+                      <Form.Label>Birthday:</Form.Label>
+                      <Form.Control
+                        type="birthday"
+                        value={birthday}
+                        onChange={(e) => setBirthday(e.target.value)}
+                        placeholder="Enter Birthday"
+                      />
+                    </Form.Group>
+                    <Button
+                      type="submit"
+                      variant="info"
+                      style={{ marginTop: "2%" }}
+                      onClick={handleSubmit}
+                    >
+                      Submit
+                    </Button>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </CardGroup>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
   );
 }
 
