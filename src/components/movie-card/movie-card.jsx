bce86cb5e9b2b0c2e71f3102b3d33f81002e13e6
PamelaKlapp <pameklapp@gmail.com>
2022-07-07 18:59:44 +0200

    added link

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,19 +3,22 @@ import PropTypes from "prop-types";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
+import { Link } from "react-router-dom";
+
 export class MovieCard extends React.Component {
   render() {
-    const { movie, onMovieClick } = this.props;
+    const { movie} = this.props;
 
     return (
       <Card>
         <Card.Img variant="top" src={movie.ImagePath} />
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
-          <Card.Text>{movie.Description}</Card.Text>
+          <Link to={`/movies/${movie._id}`}>
           <Button onClick={() => onMovieClick(movie)} variant="link">
             Open
           </Button>
+          </Link>
         </Card.Body>
       </Card>
     );
