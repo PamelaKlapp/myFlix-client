c1d271cabaa9f910159ec55d28ed3f82cb1e0221
PamelaKlapp <pameklapp@gmail.com>
2022-07-07 21:36:10 +0200

    rewrite movie vie

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export class MovieView extends React.Component {
   keypressCallback(event) {
@@ -11,43 +12,6 @@ export class MovieView extends React.Component {
 
     return (
       <div className="movie-view">
-<<<<<<< Updated upstream
-        <div className="movie-poster">
-          <img src={movie.ImagePath} height="300" />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <div className="movie-genre-name">
-          <span className="label">Genre: </span>
-          <span className="value">{movie.Genre.Name}</span>
-        </div>
-        <div className="movie-genre-description">
-          <span className="label">Genre description: </span>
-          <span className="value">{movie.Genre.Description}</span>
-        </div>
-        <div className="movie-director-name">
-          <span className="label">Director: </span>
-          <span className="value">{movie.Director.Name}</span>
-        </div>
-        <div className="movie-director-bio">
-          <span className="label">Bio: </span>
-          <span className="value">{movie.Director.Bio}</span>
-        </div>
-
-        <button
-          onClick={() => {
-            onBackClick(null);
-          }}
-        >
-          Back
-        </button>
-=======
         <Row className="justify-content-md-center margin-top">
           <Col md={4}>
             <div className="movie-poster">
@@ -104,7 +68,6 @@ export class MovieView extends React.Component {
             </Button>
           </Col>
         </Row>
->>>>>>> Stashed changes
       </div>
     );
   }
