c8178f6ea86a979eebf905bb9fd09bf7e51f70f1
PamelaKlapp <pameklapp@gmail.com>
2022-07-07 18:59:02 +0200

    added link to director and genre

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -11,6 +11,7 @@ export class MovieView extends React.Component {
 
     return (
       <div className="movie-view">
+<<<<<<< Updated upstream
         <div className="movie-poster">
           <img src={movie.ImagePath} height="300" />
         </div>
@@ -46,6 +47,64 @@ export class MovieView extends React.Component {
         >
           Back
         </button>
+=======
+        <Row className="justify-content-md-center margin-top">
+          <Col md={4}>
+            <div className="movie-poster">
+              <img src={movie.ImagePath} height="300" />
+            </div>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center margin-top">
+          <Col>
+            <div className="movie-title">
+              <span className="label">Title: </span>
+              <span className="value">{movie.Title}</span>
+            </div>
+            <div className="movie-description">
+              <span className="label">Description: </span>
+              <span className="value">{movie.Description}</span>
+            </div>
+            <div className="movie-genre-name">
+              <span className="label">Genre: </span>
+              <span className="value">{movie.Genre.Name}</span>
+            </div>
+            <div className="movie-genre-description">
+              <span className="label">Genre description: </span>
+              <span className="value">{movie.Genre.Description}</span>
+            </div>
+            <div className="movie-director-name">
+              <span className="label">Director: </span>
+              <span className="value">{movie.Director.Name}</span>
+            </div>
+            <div className="movie-director-bio">
+              <span className="label">Bio: </span>
+              <span className="value">{movie.Director.Bio}</span>
+            </div>
+          </Col>
+          <Col>
+            <Link to={`/directors/${movie.Director.Name}`}>
+              <Button variant="link">Director</Button>
+            </Link>
+
+            <Link to={`/genres/${movie.Genre.Name}`}>
+              <Button variant="link">Genre</Button>
+            </Link>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button
+              className="justify-content-md-center margin-top"
+              onClick={() => {
+                onBackClick(null);
+              }}
+            >
+              Back
+            </Button>
+          </Col>
+        </Row>
+>>>>>>> Stashed changes
       </div>
     );
   }
