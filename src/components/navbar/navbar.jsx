b6bb2dbd1ab406adb5a1baf89c58b0102bc886cf
PamelaKlapp <pameklapp@gmail.com>
2022-07-07 18:57:20 +0200

    Create navbar.jsx

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Navbar,Nav, Container, Button } from "react-bootstrap";
+import "./navbar.scss";
+ 
+export function Menubar({user}) {
+
+    const onLoggedOut = () =>{
+        localStorage.clear();
+        window.open("/", "_self");
+    }
+
+    const isAuth = () =>{
+        if(typeof window == "undefined"){
+            return false;
+        }
+        if (localStorage.getItem("token")){
+            return localStorage.getItem("token");
+        }else {
+            return false;
+        }
+    };
+
+    return (
+        <Navbar className="main-nav nav-bg-color" sticky="top" expand="lg">
+            <Container>
+                <Navbar.Brand className="navbar-logo" href="/">MyFlix MOVIE</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto">
+                        {isAuth() && (
+                            <Nav.Link href={"/users/${user}"}>{user}</Nav.Link>
+                        )}
+                        {isAuth() && (<Button variant="link" onClick={()=>{this.onLoggedOut()}}>Logout</Button>
+                        )}
+                        {!isAuth() && (<Nav.Link href="/">Sign In</Nav.Link>
+                        )}
+                        {!isAuth() && (<Nav.Link href="/register">Sign up</Nav.Link>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+
