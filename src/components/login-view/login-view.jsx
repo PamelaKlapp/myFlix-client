62af0c83fa0d93529244e55d43bc45b6aa8bc05e
PamelaKlapp <pameklapp@gmail.com>
2022-07-07 11:54:49 +0200

    add Form Validation

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -15,9 +15,35 @@ import axios from "axios";
 export function LoginView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+// Declare hook for each input
+const [ usernameErr, setUsernameErr ] = useState('');
+const [ passwordErr, setPasswordErr ] = useState('');
+
+// validate user inputs
+const validate = () => {
+  let isReq = true;
+  if(!username){
+   setUsernameErr('Username Required');
+   isReq = false;
+  }else if(username.length < 2){
+   setUsernameErr('Username must be 2 characters long');
+   isReq = false;
+  }
+  if(!password){
+   setPasswordErr('Password Required');
+   isReq = false;
+  }else if(password.length < 6){
+   setPassword('Password must be 6 characters long');
+   isReq = false;
+  }
+
+  return isReq;
+}
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const isReq = validate();
+    if(isReq){
     axios.post('https://peliapp-heroku.herokuapp.com/login', {
       Username: username,
       Password: password
@@ -29,6 +55,7 @@ export function LoginView(props) {
     .catch(e => {
       console.log('no such user')
     });
+    }
   };
   
 
@@ -57,16 +84,22 @@ export function LoginView(props) {
             <Form.Group controlId="formUsername">
               <Form.Label>Username:</Form.Label>
               <Form.Control
-                type="text"
+                type="text" 
+                placeholder="Enter username"
+                value={username}
                 onChange={(e) => setUsername(e.target.value)}
               />
+              {usernameErr && <p>{usernameErr}</p>}
             </Form.Group>
             <Form.Group controlId="formPassword">
               <Form.Label>Password:</Form.Label>
               <Form.Control
                 type="password"
+                placeholder="Enter your password"
+                value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
+              {passwordErr && <p>{passwordErr}</p>}
             </Form.Group>
             <Button
               variant="primary"
